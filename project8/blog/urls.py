48956fe4277f327deb327f0c1bee0763e05364d4
prakash chaudhary <chaudharyprakash023pc@gmail.com>
2026-08-01 20:48:35 +0545

    django tags & inheritance

--- project8/blog/urls.py
+++ project8/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from .import views
 urlpatterns=[
-     path(' ',views.blog_list,name='blog_list')
+     path('',views.blog_list,name='blog_list')
  ]
--- project8/blog/views.py
+++ project8/blog/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from datetime import datetime
 
 def blog_list(request):
+    blogs=[
+        {'title':'django basics','is_featured':True,"author":'john Doe','date':datetime(2023,10,1)},
+        {'title':'React Baics','is_featured':False,"author":'Prakash chaudhary','date':datetime(2024,10,1)},
+        {'title':'Express Basics','is_featured':False,"author":'','date':datetime(2025,10,1)}
+    ]
     context={
-        
+        'blogs':blogs,
+        "today":datetime.now(),
+        "html_code":"<h1><b>welcome to my blog</b></h1>"
     }
-return render(request,'blog/blog_list.html',context)    
+    return render(request,'blog/blog_list.html',context)    
