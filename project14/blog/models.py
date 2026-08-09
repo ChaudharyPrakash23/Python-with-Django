d13c834f30bc64f842d0ac6089b3b4f9c6b19186
prakash chaudhary <chaudharyprakash023pc@gmail.com>
2026-08-09 22:32:12 +0545

    learned django ORM query set

--- project14/blog/models.py
+++ project14/blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 class Student(models.Model):
     name=models.CharField(max_length=30)
-    age=models.IntegerField(max_length=3)
+    age=models.IntegerField()
     email=models.EmailField(unique=True)
     enrollment_date=models.DateField(auto_now_add=True)
+    city=models.CharField(max_length=30,default='unknown')
+    
+    def __str__(self):
+        return self.name
